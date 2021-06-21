5adf37d775fea1a8a49bff706db5aa274150669d
YangWeiBin <863255386@qq.com>
2021-06-22 05:42:03 +0800

    2021/06/22 05:42:03 GMT+8

--- 0001-git-push.bat
+++ 0001-git-push.bat
@@ -3,10 +3,10 @@ set GMT=GMT+8
 set DATEINTERAL=/
 set TIMEINTERAL=:
 set SPACE=" "
+set ZERO=0
 :: 1 获取日期
 set THISDATE=%DATE:~0,4%%DATEINTERAL%%DATE:~5,2%%DATEINTERAL%%DATE:~8,2%
 :: 2 如果小时是一位数H的话，将其该为0H
-set ZERO=0
 set ThISHOUR01=%TIME:~0,1%
 if "%ThISHOUR01%" == " "  (
 	set ThISHOUR01=%ZERO%%TIME:~1,1%
@@ -18,7 +18,6 @@ set ThISTIME=%ThISHOUR01%%TIMEINTERAL%%TIME:~3,2%%TIMEINTERAL%%TIME:~6,2%
 :: 4 组合后的日期时间串
 set ThISDATETIME=%THISDATE%%SPACE%%ThISTIME%
 set RESULT=%ThISDATETIME%%SPACE%%GMT%
-
 git add .
 git commit -m %RESULT%
 git push
